fecd75e16a6a3281224959656ad220c7b6264148
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2025-06-30 18:36:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,11 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAPU8E37wxO031vKegZpgvo2Y0tyuBO96c",
-	authDomain: "vedhika-61c9f.firebaseapp.com",
-	projectId: "vedhika-61c9f",
-	storageBucket: "vedhika-61c9f.firebasestorage.app",
-	messagingSenderId: "612091745025",
-	appId: "1:612091745025:web:7d9c207af4aec4daf14100",
+  authDomain: "vedhika-61c9f.firebaseapp.com",
+  projectId: "vedhika-61c9f",
+  storageBucket: "vedhika-61c9f.firebasestorage.app",
+  messagingSenderId: "612091745025",
+  appId: "1:612091745025:web:7d9c207af4aec4daf14100"
 };
 
 const app = initializeApp(firebaseConfig);
