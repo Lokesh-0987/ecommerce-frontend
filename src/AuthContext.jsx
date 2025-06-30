18fa0d9fa6527f4e19a02e11d703bd14fc0c529c
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2025-07-01 00:04:52 +0530

    Update AuthContext.jsx

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// AuthContext.jsx
 import { createContext, useContext, useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
@@ -22,28 +20,3 @@ export const AuthProvider = ({ children }) => {
 		</AuthContext.Provider>
 	);
 };
-=======
-// AuthContext.jsx
-import { createContext, useContext, useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
-
-const AuthContext = createContext();
-
-export const useAuth = () => useContext(AuthContext);
-
-export const AuthProvider = ({ children }) => {
-	const [currentUser, setCurrentUser] = useState(null);
-
-	useEffect(() => {
-		const unsub = onAuthStateChanged(auth, (user) => setCurrentUser(user));
-		return unsub;
-	}, []);
-
-	return (
-		<AuthContext.Provider value={{ currentUser }}>
-			{children}
-		</AuthContext.Provider>
-	);
-};
->>>>>>> 948b88f (Add .env setup and update .gitignore)
