0afe90832cd105cce3bdb5dbc9f92b1cc8f178dc
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2025-07-01 00:02:16 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Header from "./Home/Header";
 import Sliders from "./Home/Sliders";
 import ShopByCategory from "./Home/ShopByCategory";
@@ -20,26 +19,3 @@ function Home() {
 }
 
 export default Home;
-=======
-import Header from "./Home/Header";
-import Sliders from "./Home/Sliders";
-import ShopByCategory from "./Home/ShopByCategory";
-import LatestProducts from "./Home/LatestProducts";
-import Features from "./Home/Features";
-import Bottom from "./Home/Bottom";
-
-function Home() {
-	return (
-		<>
-			<Header />
-			<Sliders />
-			<ShopByCategory />
-			<LatestProducts />
-			<Features />
-			<Bottom />
-		</>
-	);
-}
-
-export default Home;
->>>>>>> 948b88f (Add .env setup and update .gitignore)
