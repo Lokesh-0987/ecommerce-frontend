81d8d29ecc06f9594016ad0cfec83e1ec1e71b17
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2025-06-30 21:26:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,11 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
+console.log("ENV TEST", {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  // etc...
+});
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
