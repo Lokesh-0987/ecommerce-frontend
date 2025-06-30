26b89ab7dc54ea3c503fa9216750d2ca150d5b4a
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2025-07-01 00:03:02 +0530

    Update Confirm.jsx

--- src/Confirm.jsx
+++ src/Confirm.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ConPage from "./Confirm/ConPage";
 import Bottom from "./Home/Bottom";
 import Header from "./Home/Header";
@@ -16,22 +15,3 @@ function Confirm() {
 }
 
 export default Confirm;
-=======
-import ConPage from "./Confirm/ConPage";
-import Bottom from "./Home/Bottom";
-import Header from "./Home/Header";
-
-function Confirm() {
-	return (
-		<>
-			<Header />
-			<section>
-				<ConPage />
-			</section>
-			<Bottom />
-		</>
-	);
-}
-
-export default Confirm;
->>>>>>> 948b88f (Add .env setup and update .gitignore)
