5321a762de6980ca37aa3e91bfab1a62ffb544d4
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2025-06-30 21:28:13 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { auth } from './firebase';
+
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+	apiKey: "AIzaSyAPU8E37wxO031vKegZpgvo2Y0tyuBO96c",
+  authDomain: "vedhika-61c9f.firebaseapp.com",
+  projectId: "vedhika-61c9f",
+  storageBucket: "vedhika-61c9f.firebasestorage.app",
+  messagingSenderId: "612091745025",
+  appId: "1:612091745025:web:7d9c207af4aec4daf14100"
 };
-console.log("ENV TEST", {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  // etc...
-});
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
