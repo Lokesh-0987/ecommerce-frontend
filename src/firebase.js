9714ec865c4965e55a538575aeb691b032baec21
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2025-06-30 21:24:14 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { auth } from './firebase';
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
