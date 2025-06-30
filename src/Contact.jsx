51fe9d9134a57438f064c49c8733d562f3a41d58
Lokesh Paleti <145462502+Lokesh-0987@users.noreply.github.com>
2025-07-01 00:02:43 +0530

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ContactForm from "./Contact/ContactForm";
 import Info from "./Contact/Info";
 import Bottom from "./Home/Bottom";
@@ -23,29 +22,3 @@ function Contact() {
 	);
 }
 export default Contact;
-=======
-import ContactForm from "./Contact/ContactForm";
-import Info from "./Contact/Info";
-import Bottom from "./Home/Bottom";
-import Header from "./Home/Header";
-
-function Contact() {
-	return (
-		<>
-			<Header />
-			<section className="container my-5">
-				<div className="row">
-					<div className="col-12 col-md-7">
-						<ContactForm />
-					</div>
-					<div className="col-12 col-md-5">
-						<Info />
-					</div>
-				</div>
-			</section>
-			<Bottom />
-		</>
-	);
-}
-export default Contact;
->>>>>>> 948b88f (Add .env setup and update .gitignore)
